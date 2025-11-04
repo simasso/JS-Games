1049bbf2e2aa2afd8f507643546688fb6690b643
Simone Massonie <simone.massonie@gmail.com>
2025-11-04 16:15:33 +0100

    generate options

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,7 +1,7 @@
 const options = [
-  { key: 0, short: "r", long: "rock", beats: "scissors" },
-  { key: 1, short: "p", long: "paper", beats: "rock" },
-  { key: 2, short: "s", long: "scissors", beats: "paper" },
+  { short: "r", long: "rock", beats: "scissors" },
+  { short: "p", long: "paper", beats: "rock" },
+  { short: "s", long: "scissors", beats: "paper" },
 ];
 
 function wrong_input() {
@@ -9,13 +9,39 @@ function wrong_input() {
   process.exit(2);
 }
 
-if (process.argv.length < 3) {
-  wrong_input();
+function parse_input_opt() {
+  if (process.argv.length < 3) {
+    wrong_input();
+  }
+  const input = String(process.argv[2]).toLowerCase();
+  const input_opt = options.find(
+    (opt) => opt.short === input || opt.long === input
+  );
+  if (input_opt === undefined) {
+    wrong_input();
+  }
+  return input_opt;
 }
-const input = String(process.argv[2]).toLowerCase();
-const input_opt = options.find(
-  (opt) => opt.short === input || opt.long === input
-);
-if (input_opt === undefined) {
-  wrong_input();
+
+function generate_opt() {
+  const numFloatSmaller100 = Math.random() * 100;
+  const numSmallThanOrEqualTo100 = Math.round(numFloatSmaller100);
+  const numBetween0And2 = numSmallThanOrEqualTo100 % 3;
+  console.log(numBetween0And2);
+  return options[numBetween0And2];
+}
+
+function toCapital(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+const your_opt = parse_input_opt();
+const my_opt = generate_opt();
+console.log(`You say "${your_opt.long}" and I say "${my_opt.long}".`);
+if (your_opt.beats === my_opt.long) {
+  console.log(`${toCapital(your_opt.long)} beats ${my_opt.long}. You won!`);
+} else if (my_opt.beats === your_opt.long) {
+  console.log(`${toCapital(my_opt.long)} beats ${your_opt.long}. I won!`);
+} else {
+  console.log("No winner!");
 }
